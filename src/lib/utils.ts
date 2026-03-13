ece544aefda69455f5f134d5021c4558a789a0b1
Timothé <timothe.fardella@viacesi.fr>
2026-03-13 20:08:06 +0100

    refactor : show years and months

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -14,5 +14,9 @@ export function timeAgo(dateStr: string): string {
   const days = Math.floor(hrs / 24)
   if (days < 30) return `${days}d ago`
   const months = Math.floor(days / 30)
-  return `${months}mo ago`
+  if (months < 12) return `${months}mo ago`
+  const years = Math.floor(months / 12)
+  const remainingMonths = months % 12
+  if (remainingMonths === 0) return `${years}y ago`
+  return `${years}y ${remainingMonths}mo ago`
 }
